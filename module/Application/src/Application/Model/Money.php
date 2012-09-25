f98a2bee9d0c44d96ba313e67163295942563811
Libor Jonát <jonatlib@fit.cvut.cz>
2012-09-25 16:20:11 +0200

    Dont group by date.

--- module/Application/src/Application/Model/Money.php
+++ module/Application/src/Application/Model/Money.php
@@ -61,7 +61,7 @@ class Money extends \Zend\Db\TableGateway\TableGateway {
                     'date' => 'date',
                 ))
                 ->join(array('c' => 'Category'), 'category = c.id', array('categName' => 'name'))
-                ->group('Money.date')->group('c.id');
+                ->group('c.id');
         $data = $this->getAdapter()->query($select->getSqlString($this->getAdapter()->getPlatform()), Db\Adapter\Adapter::QUERY_MODE_EXECUTE);
         return $data;
     }
